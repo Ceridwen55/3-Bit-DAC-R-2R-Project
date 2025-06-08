7966b561b4092f8953da2f92897af485ce8207c3
Zulkan <zulkanzaqzuq@gmail.com>
2025-06-08 18:05:05 +0700

    Setting up the software

---
+++ MainCode.c
@@ -0,0 +1,16 @@
+#include <stdint.h>
+#include <stdio.h>
+
+
+
+
+//***PROJECT DESIGN***//
+
+/*
+1. Create DAC 2R-R 3 bit 
+2. Using 5 30 kOhm Resistors, and 2 15 kOhm Resistors
+3. 3 bit sets at PK1,PK2,PK3
+4. Using Audio Jack Female 3,5 mm, resistor series with the Tip, and GND MCU to the Sleeve of the female audio jack
+
+
+*/
